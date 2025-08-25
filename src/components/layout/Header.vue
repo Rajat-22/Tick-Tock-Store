38756dfcecbbd920a8d9f6c47ab3a93d919ef95e
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-08-25 14:47:09 +0530

    adjust header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,18 +1,26 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+     <img src="../../assets/watch.jpg" style="width: 50px; height: 50px;" class="mx-3" alt="TickTock Store"></img>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+           <router-link class="nav-link active" aria-current="page"
+           :to="{name: APP_ROUTE_NAMES.HOME}"
+           >Home</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+           <router-link class="nav-link active" aria-current="page"
+           :to="{name: APP_ROUTE_NAMES.PRODUCT_LIST}"
+           >Product</router-link>
         </li>
+      </ul>
+
+      <ul class="d-flex navbar-nav">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
@@ -25,14 +33,25 @@
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+           <router-link class="nav-link active" aria-current="page"
+           :to="{name: APP_ROUTE_NAMES.SIGN_IN}"
+           >SignIn</router-link>
+        </li>
+        <li class="nav-item">
+           <router-link class="nav-link active" aria-current="page"
+           :to="{name: APP_ROUTE_NAMES.SIGN_UP}"
+           >SignUp</router-link>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      
     </div>
   </div>
 </nav>
 </template>
+
+<script setup>
+import { APP_ROUTE_NAMES } from '@/constants/routerName';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+</script>
