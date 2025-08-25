96d8f612fedd34bedddee324134d1e42d781ccb1
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-08-25 15:04:27 +0530

    added theme in the application

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -23,13 +23,11 @@
       <ul class="d-flex navbar-nav">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            <i class="bi bi-laptop"></i>
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><button @click="themeStore.setTheme('light')" class="dropdown-item"><i class="bi bi-sun"></i>&nbsp; Light</button></li>
+        <li><button @click="themeStore.setTheme('dark')"class="dropdown-item"><i class="bi bi-moon-stars-fill"></i>&nbsp; Dark</button></li>
           </ul>
         </li>
         <li class="nav-item">
@@ -52,6 +50,8 @@
 <script setup>
 import { APP_ROUTE_NAMES } from '@/constants/routerName';
 import { useRouter } from 'vue-router';
+import { useThemeStore } from '@/store/themeStore';
 
+const themeStore = useThemeStore()
 const router = useRouter()
 </script>
