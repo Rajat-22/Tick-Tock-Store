dcf2510a5a5c82f0a51748a82424e12092eee610
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-08-25 13:59:29 +0530

    footer in action

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,1 +1,5 @@
-<template>Footer </template>
+<template>
+    <footer class="border-top footer text-muted p-2 bg-body-tertiary">
+        <div class="text-center"> Made by Rajat Sharma <i class="bi bi-person-standing"></i></div>
+    </footer>
+</template>
