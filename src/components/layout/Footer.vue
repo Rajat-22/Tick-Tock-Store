6f6f4fcb06cb6aef51eff771fcc6c94a3b03703c
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-08-25 15:39:22 +0530

    footer adusted

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,24 @@
 <template>
-    <footer class="border-top footer text-muted p-2 bg-body-tertiary">
-        <div class="text-center"> Made by Rajat Sharma <i class="bi bi-person-standing"></i></div>
-    </footer>
+    <footer class="border-top footer text-muted p-3 bg-body-tertiary">
+  <div class="container">
+    <div class="row align-items-center">
+      <!-- Left side -->
+      <div class="col text-start">
+          <h4 class="mb-1 text-info fw-bold"><i class="bi bi-watch"></i>&nbsp; Tick-Tock Store</h4>
+          <p class="mb-0 fst-italic text-warning">“Keeping You On Time, Every Time.”</p>
+        </div>
+
+      <!-- Right side -->
+      <div class="col text-end">
+        <span class="text-muted">
+            &copy; <span class="text-primary fw-semibold">{{new Date().getFullYear()}}</span> Tick-Tock Store | All Rights Reserved.
+        </span>  
+      </div>
+    </div>
+  </div>
+</footer>
 </template>
+
+<script setup>
+
+</script>
