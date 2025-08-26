b96f51a1942dc6f528103d575839bf7bcda3f5b0
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-08-26 19:17:22 +0530

    create auth store of firebase

---
+++ src/store/authStore.js
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+import { ref, reactive } from "vue";
+import { db, auth } from "@/utility/firebaseConfig";
+import { doc, setDoc } from "firebase/firestore";
+import {  createUserWithEmailAndPassword } from 'firebase/auth'
+
+export const useAuthStore = defineStore('authStore', () => {
+    const user = ref(null)
+    const error = ref(null)
+    const isLoading = ref(false)
+
+    const signUpUser = async(email, password) => {
+        isLoading.value = true
+        try {
+            const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+            user.value = userCredentials.user
+            error.value = null
+        } catch (err) {
+            error.value = err.message
+            throw err
+        } finally {
+            isLoading.value = false
+        }
+    }
+
+    return { user, error,isLoading, signUpUser}
+})
