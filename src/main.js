6afd61399a67091638447ca044873b360af2244f
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-08-28 13:12:10 +0530

    themstore added to main

--- src/main.js
+++ src/main.js
@@ -7,15 +7,27 @@ import router from './router/route'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { useThemeStore } from './store/themeStore'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-let app;
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+
+const themeStore = useThemeStore()
+if(themeStore.theme){
+    const bodyElement = document.body
+    bodyElement.setAttribute('data-bs-theme', themeStore.theme)
+}
+
+let isMounted = false
 
 onAuthStateChanged(auth, async(user) => {
-    if(!app){
-app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+    if(!isMounted){
+    app.mount('#app')
+    isMounted = true
     }
 })
