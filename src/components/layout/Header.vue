890531482bfa8ee8cac3abb5f69384afd0c56cb8
Rajat Sharma <rajatsharma221098@gmail.com>
2025-11-03 19:02:52 +0530

    header UI update

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,64 +1,95 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-     <img src="../../assets/tick-tock.jpg" style="width: 120px; height: 50px;" class="mx-3" alt="TickTock Store"></img>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
-           <router-link class="nav-link active" aria-current="page"
-           :to="{name: APP_ROUTE_NAMES.HOME}"
-           >Home</router-link>
-        </li>
-        <li class="nav-item">
-           <router-link class="nav-link active" aria-current="page"
-           :to="{name: APP_ROUTE_NAMES.PRODUCT_LIST}"
-           >Product</router-link>
-        </li>
-        <li class="nav-item">
-           <router-link class="nav-link active" aria-current="page"
-           :to="{name: APP_ROUTE_NAMES.CONTACT_US}"
-           >Contact Us</router-link>
-        </li>
-      </ul>
+  <nav
+    ref="navbarRef"
+    class="navbar navbar-expand-lg premium-navbar fixed-top"
+    :class="{ scrolled: isScrolled }"
+  >
+    <div class="container">
+      <router-link :to="{ name: APP_ROUTE_NAMES.HOME }" class="navbar-brand d-flex align-items-center">
+        <img
+          src="@/assets/tick-tock.jpg"
+          alt="TickTock Store"
+          class="logo-img me-2"
+          :class="{ 'logo-shrink': isScrolled }"
+        />
+      </router-link>
 
-      <ul class="d-flex navbar-nav">
-        <li class="nav-link" v-if="authStore.isAuthenticated">
-          Welcome, {{ authStore.user?.email }}
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-laptop"></i>
-          </a>
-          <ul class="dropdown-menu">
-            <li><button @click="themeStore.setTheme('light')" class="dropdown-item"><i class="bi bi-sun"></i>&nbsp; Light</button></li>
-        <li><button @click="themeStore.setTheme('dark')"class="dropdown-item"><i class="bi bi-moon-stars-fill"></i>&nbsp; Dark</button></li>
-          </ul>
-        </li>
-        <li class="nav-item" v-if="!authStore.isAuthenticated">
-           <router-link class="nav-link active" aria-current="page"
-           :to="{name: APP_ROUTE_NAMES.SIGN_IN}"
-           >SignIn</router-link>
-        </li>
-        <li class="nav-item" v-if="!authStore.isAuthenticated">
-           <router-link class="nav-link active" aria-current="page"
-           :to="{name: APP_ROUTE_NAMES.SIGN_UP}"
-           >SignUp</router-link>
-        </li>
-        <li class="nav-item" v-if="authStore.isAuthenticated">
-          <button @click="[authStore.signOutUser(), router.push({name: APP_ROUTE_NAMES.HOME})]" class="nav-link">Sign Out</button>
-        </li>
-      </ul>
-      
+      <button
+        class="navbar-toggler border-0 custom-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="bi bi-list"></i>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto align-items-lg-center">
+          <li class="nav-item mx-2">
+            <router-link class="nav-link" :to="{ name: APP_ROUTE_NAMES.HOME }">Home</router-link>
+          </li>
+          <li class="nav-item mx-2">
+            <router-link class="nav-link" :to="{ name: APP_ROUTE_NAMES.PRODUCT_LIST }">Products</router-link>
+          </li>
+          <li class="nav-item mx-2">
+            <router-link class="nav-link" :to="{ name: APP_ROUTE_NAMES.CONTACT_US }">Contact</router-link>
+          </li>
+
+          <li class="nav-item dropdown mx-2">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i class="bi bi-laptop"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end shadow-sm">
+              <li>
+                <button @click="themeStore.setTheme('light')" class="dropdown-item">
+                  <i class="bi bi-sun"></i> Light
+                </button>
+              </li>
+              <li>
+                <button @click="themeStore.setTheme('dark')" class="dropdown-item">
+                  <i class="bi bi-moon-stars-fill"></i> Dark
+                </button>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item mx-2" v-if="!authStore.isAuthenticated">
+            <router-link class="nav-link" :to="{ name: APP_ROUTE_NAMES.SIGN_IN }">Sign In</router-link>
+          </li>
+          <li class="nav-item mx-2" v-if="!authStore.isAuthenticated">
+            <router-link
+              class="nav-link"
+              :to="{ name: APP_ROUTE_NAMES.SIGN_UP }"
+            >
+              Sign Up
+            </router-link>
+          </li>
+          <li class="nav-item mx-2" v-if="authStore.isAuthenticated">
+            <button
+              class="btn btn-outline-danger rounded-pill px-3 py-1"
+              @click="[authStore.signOutUser(), router.push({ name: APP_ROUTE_NAMES.HOME })]"
+            >
+              Sign Out
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
+
 <script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { APP_ROUTE_NAMES } from '@/constants/routerName';
 import { useRouter } from 'vue-router';
 import { useThemeStore } from '@/store/themeStore';
@@ -67,4 +98,21 @@ import { useAuthStore } from '@/store/authStore';
 const authStore = useAuthStore()
 const themeStore = useThemeStore()
 const router = useRouter()
+
+const isScrolled = ref(false)
+const navbarRef = ref(null)
+
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 30
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+
 </script>
+
